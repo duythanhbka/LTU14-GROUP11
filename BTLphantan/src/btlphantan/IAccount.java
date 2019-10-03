8c4b5f770b242272899a606620fad39f57234938
Nguyen Dinh Kieu <dinhkieu153@gmail.com>
2019-10-03 10:22:06 +0700

    Add RMI

---
+++ BTLphantan/src/btlphantan/IAccount.java
@@ -0,0 +1,18 @@
+package btlphantan;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import com.bean.User;
+
+/**
+ * @author framgiavn
+ */
+public interface IAccount extends Remote {
+
+    /**
+     * @return
+     * @throws RemoteException
+     */
+    public User getUser() throws RemoteException;
+}
---
+++ BTLphantan/src/btlphantan/RMIServer.java
@@ -0,0 +1,31 @@
+package btlphantan;
+
+import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+import btlphantan.IAccount;
+
+public class RMIServer {
+    public static void main(String args[]) {
+
+        try {
+            IAccount rAccount = new AccountServiceImpl();
+
+            LocateRegistry.createRegistry(6789);
+
+			// Đăng ký đối tượng này với rmiregistry
+            Naming.bind("rmi://192.168.1.230:6789/SeptemberRMI", rAccount);
+
+            System.out.println(">>>>>INFO: RMI Server started!!!!!!!!");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (AlreadyBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+}
